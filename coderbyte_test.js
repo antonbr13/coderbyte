d65a657d499573923524c9ae63132aaf33df0de6
Anton Bredl <antonbr13@gmail.com>
2015-10-14 22:07:52 -0600

    not much work - just committing for practice and to save out for the night

--- coderbyte_test.js
+++ coderbyte_test.js
@@ -1,5 +1,29 @@
-function last {
-	
+function last() {
+	return "I am last!";
 }
 
-console.log(myLoop(3, 4));
+console.log('last() should return the string "I am last!" -->', last() === "I am last!");
+
+function isEven(n) {
+	if (n % 2 === 0) return true;
+	return false;
+}
+
+console.log('isEven(2) should return true -->', isEven(2) === true)
+console.log('isEven(3) should return false -->', isEven(3) === false)
+
+
+// Using the JavaScript language, have the function firstReverse(str) take the str parameter being passed and return the string in reversed order. 
+
+var afterMath = [];
+
+function firstReverse(str) {
+	var splitStr = str.split("");
+	
+	for(var i = 0; i < 0; i++) {
+		return str.split("").reverse().join("");
+	}
+	
+	
+}
+console.log(firstReverse("hey"));
